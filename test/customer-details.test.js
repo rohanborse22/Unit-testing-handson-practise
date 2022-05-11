c7f7be40b0687649b5ab6c13a18a1ff5870665ff
DKATZZZ <92221627+DKATZZZ@users.noreply.github.com>
2022-05-11 22:04:09 +0530

    commit customer details

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,6 +1,8 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import { localize } from '@lion/localize';
 import { stub } from 'sinon';
-import '../src/Customer/Customer-details.js';
+import  '../src/Customer/Customer-details.js';
+import Sinon from 'sinon';
 
 describe('customer details', () => {
   // Write test cases inside this block
