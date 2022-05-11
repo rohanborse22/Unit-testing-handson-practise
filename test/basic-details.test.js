35bb126e5b652e0db0eb910ccce463c275f515f5
DKATZZZ <92221627+DKATZZZ@users.noreply.github.com>
2022-05-11 22:03:15 +0530

    commit basic details

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,17 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import  Sinon  from 'sinon';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+
+  
+  it('checks for spy function',()=>{
+    const el=fixture(html`<basic-details></basic-details>`);
+    const myspy=Sinon.spy(el,'_toDashboard');
+    form.click();
+    expect(myspy.calledOnce).to.be.true;
+      });
 });
