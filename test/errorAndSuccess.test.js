6651771ce2e7ee4b259b267a38b5744c8084306d
Rohan <rohanborse22@gmail.com>
2023-02-25 15:17:27 +0530

    Added unit test cases for error and success

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -1,12 +1,37 @@
 import { html, fixture, expect } from '@open-wc/testing';
-// import { stub } from 'sinon';
+import sinon, { spy } from 'sinon';
 import '../src/SuccessAndError/Success.js';
 import '../src/SuccessAndError/Error.js';
 
-describe('Success screen ', () => {
-  // Write test cases inside this block
+const success = await fixture(html`<loan-success></loan-success>`)
+const error = await fixture(html`<loan-error></loan-error>`)
+
+describe('Success screen', () => {
+    beforeEach(function () {
+        sinon.restore();
+    });
+
+    it('Test call _toHome method when home button is clicked', async () => {
+        const toHomeStub = sinon.stub(success, '_toHome');
+        const homeBtn = success.shadowRoot.querySelector('.home-btn');
+        success.requestUpdate();
+        await success.updateComplete;
+        homeBtn.click();
+        expect(toHomeStub).to.have.callCount(1);
+    })
 });
 
-describe('error screen', () => {
-  // Write test cases inside this block
+describe('Error screen', () => {
+    beforeEach(function () {
+        sinon.restore();
+    });
+
+    it('Test call _toHome method when home button is clicked', async () => {
+        const toHomeStub = sinon.stub(error, '_toHome');
+        const homeBtn = error.shadowRoot.querySelector('.home-btn');
+        error.requestUpdate();
+        await error.updateComplete;
+        homeBtn.click();
+        expect(toHomeStub).to.have.callCount(1);
+    })
 });
