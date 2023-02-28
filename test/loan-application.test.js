932f0031a02bb2b16565689fec313488394080eb
Rohan <rohanborse22@gmail.com>
2023-02-28 10:29:12 +0530

    added unit test cases for app & emi details

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -1,7 +1,15 @@
 import { html, fixture, expect } from '@open-wc/testing';
-
 import '../loan-application.js';
 
+let element;
 describe('LoanApplication', () => {
-  // Write test cases inside this block
+
+  beforeEach(async () => {
+    element = await fixture(html`<loan-application></loan-application>`);
+  });
+
+  // Test loan-application is render
+  it('should check loan-application is render', async () => {
+    expect(element).to.be.accessible();
+  });
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -1,6 +1,58 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import Sinon , {spy,stub} from 'sinon'
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
 describe('Loan EMI details', () => {
-  // Write test cases inside this block
+
+  let El;
+
+  before(async ()=> {
+    localStorage.setItem('emi', JSON.stringify({
+      interestRate : '2%',
+      monthlyEMI : 10,
+      principal : 200,
+      interest : 150,
+      totalAmount : 400
+    }))
+
+    El = await fixture(html`<loanemi-details></loanemi-details>`);
+
+    El.requestUpdate();
+  })
+
+
+  beforeEach(()=>{
+    Sinon.restore();
+  })
+
+  it('checks for accessibility of component', () => {
+    expect(El).to.be.accessible;
+  });
+  
+  it("should trigger _toBasicDetails() when cancel button is clicked", async () => {
+    const basicDetailStub = stub(El,'_toBasicDetails');
+
+    El.requestUpdate();
+    await El.updateComplete;
+
+    const cancelBtn = El.shadowRoot.querySelector('.cancel-btn');
+    cancelBtn.click();
+
+    expect(basicDetailStub).to.have.callCount(1);
+  })
+
+
+  it("should trigger _toCustomer() when continue button is clicked", async () => {
+
+    const toCustomerStub = stub(El,'_toCustomer');
+
+    El.requestUpdate();
+    await El.updateComplete;
+
+    const continueBtn = El.shadowRoot.querySelector('.continue-btn');
+    continueBtn.click();
+
+    expect(toCustomerStub).to.have.callCount(1);
+
+  })
 });
